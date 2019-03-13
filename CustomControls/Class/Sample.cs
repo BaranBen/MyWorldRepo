8b57a1d27e6c1cd42b578653ff2f5c9902f08791
Barancan Genç <barancan.genc@hotmail.com>
2019-03-13 10:07:42 +0300

    Proje Taşıma

---
+++ CustomControls/Class/Sample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorld.CustomControls.Class
+{
+    class FileLog : AProperties,ILogger
+    {
+        public virtual void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class DatabaseLog : ILogger
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class FTPLog : ILogger
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class SmsLog : ILogger
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+    abstract class AProperties
+    {
+        string LogMessage { get; set; }
+    }
+}
---
+++ CustomControls/Window/MyWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace MyCustomControls
+{
+    [ContentProperty("_Content")]
+    public partial class MyWindow : UserControl
+    {
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string _Header
+        {
+            get { return this.txtHeader.Text; }
+            set { this.txtHeader.Text = value; }
+        }
+
+       
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            if (oldContent != null)
+                throw new NotSupportedException("You can not. Input content again..");
+        }
+    }
+    
+}
